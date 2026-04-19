ec2dda260721772ec063e5e85e57d469324173f3
xplod24 <xplodjustboom@gmail.com>
2026-04-19 13:56:09 +0200

    Lab03EntitiesTest

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,14 +1,27 @@
 package pl.wsb.fitnesstracker.event;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
 
-// TODO: Define the Event entity with appropriate fields and annotations
 @Entity
-@Table
+@Table(name = "event")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Event {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String location;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
 }
